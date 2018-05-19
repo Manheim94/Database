5fd04e28b385fc8ebf47afe02b7671bde6d1d2c6
Manheim <ymh2ee@outlook.com>
2018-05-19 15:08:10 -0700

    my first commit

--- hw0/hw0.sql
+++ hw0/hw0.sql
@@ -1,5 +1,7 @@
 /*This file is a test file. You will never add new files to the repository in your homework assignments, and always modify designated files.*/
 
 /*TODO: Fill Something Here */
+This is my first commit for cs186!
+
 
 /*The rest of the file continues here*/
